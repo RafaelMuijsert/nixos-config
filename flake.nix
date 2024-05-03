38e49f49a26be73236d2f1b476bbcdce426bd159
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-05-03 18:37:53 +0200

    Add home manager to flake inputs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs }: {
