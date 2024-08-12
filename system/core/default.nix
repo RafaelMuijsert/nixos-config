d8b21a752e00c9253dac3866b9be28e033dae4d3
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-08-12 12:00:05 +0200

    Add core system packages

--- system/core/default.nix
+++ system/core/default.nix
@@ -1,4 +1,11 @@
+{ nixpkgs-stable, nixpkgs-unstable, ... }:
 {
+  environment.systemPackages = with nixpkgs-stable; [
+    helix
+    wget
+    git
+  ];
+
   imports = [
     ./locale.nix
     ./nix.nix
