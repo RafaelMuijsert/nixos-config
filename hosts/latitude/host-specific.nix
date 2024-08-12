27973063e19e950b9e0a001e96f86d8b1bf86cf0
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-08-12 10:11:42 +0200

    Add Latitude host-specific configuration

---
+++ hosts/latitude/host-specific.nix
@@ -0,0 +1,13 @@
+{  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+ # Use NetworkManager for networking.
+  networking = {
+    hostName = "latitude";
+    networkmanager.enable = true;
+  };
+  
+  system.stateVersion = "24.05"; 
+}
