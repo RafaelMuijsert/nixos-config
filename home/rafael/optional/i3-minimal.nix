53a383af12e8a5b43c61fd02641bd370fea974c3
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-08-30 00:28:08 +0200

    Set i3 borders

--- home/rafael/optional/i3-minimal.nix
+++ home/rafael/optional/i3-minimal.nix
@@ -18,6 +18,26 @@
           border = 1;
           titlebar = false;
         };
+        fonts = {
+          names = [ "Terminess Nerd Font Mono" ];
+          size = 9.0;
+        };
+        colors = {
+          focused = {
+            background = "#000000";
+            border = "#484848";
+            childBorder = "#484848";
+            indicator = "#484848";
+            text = "#ffffff";
+          };
+          unfocused = {
+            background = "#000000";
+            border = "#000000";
+            childBorder = "#000000";
+            indicator = "#000000";
+            text = "#ffffff";
+          };
+        };
         bars = [
           {
             position = "top";
