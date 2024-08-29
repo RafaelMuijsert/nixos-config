a67343003bef30aa78f788b526ea9662f807b611
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-08-30 00:04:29 +0200

    Hide window title bar

--- home/rafael/optional/i3-minimal.nix
+++ home/rafael/optional/i3-minimal.nix
@@ -14,7 +14,10 @@
       enable = true;
       config = {
         modifier = "Mod4";
-        window.border = 1;
+        window = {
+          border = 1;
+          titlebar = false;
+        };
         bars = [
           {
             position = "top";
