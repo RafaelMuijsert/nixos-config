34707655f2c324b757ae03215a5e52f3cfd97e40
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-09-28 23:36:44 +0200

    Remove plymouth due to stability concerns

--- system/core/settings/boot.nix
+++ system/core/settings/boot.nix
@@ -1,12 +1,5 @@
 {pkgs, ...}: {
   boot = {
-    plymouth = {
-      enable = true;
-      theme = "nixos-bgrt";
-      themePackages = with pkgs; [
-        nixos-bgrt-plymouth
-      ];
-    };
     consoleLogLevel = 3;
     initrd.verbose = true;
   };
--- system/users/rafael/default.nix
+++ system/users/rafael/default.nix
@@ -6,7 +6,7 @@
   users.users.rafael = {
     description = "Rafael Alexander Muijsert";
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel" "networkmanager"];
     shell = pkgs.fish;
   };
   programs.fish.enable = true;
