9028b4bcd69a3434e1e5463dc2df76e879964018
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-09-28 10:55:39 +0200

    Move home.nix to default.nix

--- home/rafael/core/default.nix
+++ home/rafael/core/default.nix
@@ -1,4 +1,20 @@
 {inputs, ...} @ attrs: {
   imports = [inputs.home-manager.nixosModules.default];
-  home-manager.users.rafael = import ./home.nix {inherit attrs;};
+  home-manager.users.rafael = {
+    home = {
+      username = "rafael";
+      homeDirectory = "/home/rafael";
+      stateVersion = "24.11";
+      packages = with attrs.nixpkgs-unstable; [
+        fishPlugins.hydro
+        (nerdfonts.override {fonts = ["CascadiaCode" "Terminus"];})
+      ];
+    };
+    programs.home-manager.enable = true;
+    imports = builtins.concatMap (dir: attrs.path-utils.readDirectory dir) [
+      ./programs
+      ./themes
+      ./settings
+    ];
+  };
 }
