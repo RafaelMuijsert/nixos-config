8b549dee3e5301e09d08bc2b413b6507bd29f43c
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-09-29 12:18:28 +0200

    Add driver support for hardware video decoding to EliteBook host

--- hosts/elite/host-specific.nix
+++ hosts/elite/host-specific.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
@@ -13,6 +14,14 @@
   # Use the i3-minimal desktop.
   desktop.i3-minimal.enable = true;
   desktop.i3-minimal.isLaptop = true;
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      onevpl-intel-gpu
+      intel-media-driver
+    ];
+  };
 
   system.stateVersion = "24.05";
 }
