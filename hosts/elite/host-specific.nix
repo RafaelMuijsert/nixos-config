74fe86ce1b3df4deafde1098916631f101f4929a
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-09-29 21:23:15 +0200

    Revert to minimal theme

--- hosts/elite/host-specific.nix
+++ hosts/elite/host-specific.nix
@@ -14,8 +14,8 @@
   desktop = {
     # Use the i3-minimal desktop.
     i3-minimal.enable = true;
-    # Use the catppuccin theme.
-    theme = import ../../themes/catppuccin;
+    # Use the minimal theme.
+    theme = import ../../themes/minimal;
   };
   # Enable laptop-specific configuration.
   isLaptop = true;
