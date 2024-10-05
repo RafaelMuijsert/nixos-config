99f625da79296879d5b5f324d5edee608457f550
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-10-05 21:52:29 +0200

    Fix missing mkIf

--- home/rafael/academia/symlink.nix
+++ home/rafael/academia/symlink.nix
@@ -1,8 +1,10 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   # Create a symlink to the current semester in university.
-  home.file = {
-    "Uni".source = config.lib.file.mkOutOfStoreSymlink 
-      "${config.home.homeDirectory}/Documents/Academia/BSc-Computer-Science/Year 2/Semester 1";
-  };
+  config = lib.mkIf osConfig.academia {
+    home.file = {
+      "Uni".source = config.lib.file.mkOutOfStoreSymlink 
+        "${config.home.homeDirectory}/Documents/Academia/BSc-Computer-Science/Year 2/Semester 1";
+    };
+  }
 }
