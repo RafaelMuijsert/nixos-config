71c71db18bfd2d7002eadd60cd7c4e0fe5ec4ce9
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-10-05 22:11:53 +0200

    Remove redundant configuration

--- hosts/elite/rafael.nix
+++ hosts/elite/rafael.nix
@@ -1,9 +1,6 @@
 {pkgs, ...}: {
   home-manager.users.rafael = {
     home.packages = with pkgs; [
-      # Typst
-      typst
-      typst-lsp
       # C Development
       clang-tools
       gnumake
