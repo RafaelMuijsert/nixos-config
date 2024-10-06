2896cc9aa31753e0dee83cebbfcdf1e9c627d810
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-10-07 00:07:50 +0200

    Disable overrideDevices

--- system/settings/sync.nix
+++ system/settings/sync.nix
@@ -1,6 +1,7 @@
 {
   services.syncthing = {
-    overrideDevices = true;
+    # https://github.com/NixOS/nixpkgs/issues/326704
+    overrideDevices = false;
     overrideFolders = true;
     settings.devices = {
       "one" = {id = "MBKWEDD-ZKFPJL3-XPV7IVG-D04N4M7-ALQJ4BG-GNBZXNU-7ZVP3NW-VQZC0AE";};
