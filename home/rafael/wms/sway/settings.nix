0220a65590cb618751e3b59044b2b58b29939d7d
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-10-30 10:59:19 +0100

    Disable swayidle

--- home/rafael/wms/sway/settings.nix
+++ home/rafael/wms/sway/settings.nix
@@ -24,29 +24,11 @@
         border = 1;
         titlebar = false;
       };
-      startup = let 
-        lockScreen = pkgs.writeShellApplication {
-          name = "sway-lockscreen";
-          text = ''
-              #!/bin/sh
-              swayidle -w \
-                timeout 30 "swaymsg output '*' dpms off" \
-                resume "swaymsg output '*' dpms on" &
-
-              # Lock screen
-              ${pkgs-unstable.hyprlock}/bin/hyprlock
-              pkill --newest swayidle
-          '';
-        };
-      in [
-        { command = "swayidle -w timeout 180 '${lockScreen}/bin/sway-lockscreen'"; }
-      ];
     };
     extraConfig = lib.mkIf osConfig.isLaptop (
       lib.concatLines [
         "bindswitch --reload --locked lid:off output eDP-1 enable"
-
-        "bindswitch --reload --locked lid:on output eDP-1 disable; exec ${pkgs-unstable.hyprlock}/bin/hyprlock"
+        "bindswitch --reload --locked lid:on output eDP-1 disable"
       ]
     );
   };
