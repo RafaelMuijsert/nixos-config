1f50f34820e3887914c5b8f1fa75a74082e216ce
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-01-18 10:50:57 +0100

    Configure waybar modules

--- home/rafael/programs/waybar.nix
+++ home/rafael/programs/waybar.nix
@@ -1,11 +1,56 @@
 {
   programs.waybar = {
-    settings = {
-      mainBar = {
-        height = 30;
-        modules-left = ["hyprland/workspaces" "systemd-failed-units"];
-        modules-center = ["hyprland/window"];
-        modules-right = ["tray" "mpd" "wireplumber" "network" "memory" "battery" "clock"];
+    settings.mainBar = {
+      height = 30;
+      modules-left = ["hyprland/workspaces" "systemd-failed-units"];
+      modules-center = ["hyprland/window"];
+      modules-right = ["tray" "mpd" "wireplumber" "network" "memory" "battery" "clock"];
+
+      "hyprland/window" = {
+        "max-length" = 80;
+        "rewrite" = {
+          "(.*) — Mozilla Firefox" = "󰈹 $1";
+          "(.*) - fish" = "❯ $1";
+        };
+      };
+      "tray" = {
+        
+      };
+      "mpd" = {
+        "format" = "{stateIcon} {elapsedTime:%M:%S} / {totalTime:%M:%S}";
+        "max-length" = 40;
+        "state-icons" = {
+          "paused" = "󰏤";
+          "playing" = "󰝚";
+        };
+      };
+      "wireplumber" = {
+        "format-muted" = "󰝟 Muted";
+        "format-icons" = ["󰕿" "󰖀" "󰕾"];
+        "format" = "{icon} {volume}%";
+      };
+      "network" = {
+        "format-wifi" = "{icon} {signalStrength}% {essid}";
+        "tooltip-format-wifi" = "{ipaddr} @ {ifname} ({frequency} GHz)";
+
+        "format-icons" = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
+      };
+      "memory" = {
+        format = " {}%";
+      };
+      "battery" = {
+        interval = 60;
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+        format = "{icon} {capacity}%";
+        format-icons = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        max-length = 25;
+      };
+
+      "clock" = {
+        format = " {:%F %R}";
       };
     };
     style = ''
@@ -13,5 +58,6 @@
         font-family: monospace; 
       }
     '';
+
   };
 }
