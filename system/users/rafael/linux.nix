d58dadcc7afe2169bafe82e8b711d23e263f9c3a
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-14 19:00:59 +0100

    fix: do not use sops for setting user password

--- system/users/rafael/linux.nix
+++ system/users/rafael/linux.nix
@@ -5,13 +5,10 @@
   home-manager,
   ...
 }: {
-  sops.secrets.password-rafael.neededForUsers = true;
-  users.mutableUsers = false;
 
   users.users.rafael = {
     description = "Rafael Alexander Muijsert";
     isNormalUser = true;
-    hashedPasswordFile = config.sops.secrets.password-rafael.path;
     extraGroups = ["wheel" "networkmanager"];
     shell = pkgs.fish;
     home = "/home/rafael";
