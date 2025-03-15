f58c7dee42589aae695225dac446238dd4816e10
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-15 20:42:30 +0100

    feat: host immich on one

--- hosts/one/host-specific.nix
+++ hosts/one/host-specific.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, pkgs-unstable, ... }:
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
@@ -10,6 +10,17 @@
     hostName = "one";
     networkmanager.enable = true;
     firewall.allowedTCPPorts = [ 80 443 ];
+  };
+
+  # Immich
+  services.immich = {
+    enable = true;
+    mediaLocation = "/mnt/data/Photos/";
+    package = pkgs-unstable.immich;
+    port = 2283;
+    settings = {
+      server.externalDomain = "https://photos.muijsert.org";
+    };
   };
 
   # Portfolio website
@@ -33,18 +44,34 @@
   # Reverse proxy
   services.nginx = {
     enable = true;
+    recommendedProxySettings = true;
     virtualHosts = {
       "muijsert.org" = {
         enableACME = true;
         forceSSL = true;
 
         locations."/".proxyPass = "http://localhost:3000";
+
+        serverName = "muijsert.org";
         serverAliases = [
           "www.muijsert.org"
         ];
+      };
+      "photos.muijsert.org" = {
+        enableACME = true;
+        forceSSL = true;
+
+        locations."/" = {
+          proxyPass = "http://localhost:2283";
+          proxyWebsockets = true;
+        };
+
+        serverName = "photos.muijsert.org";
       };
     };
   };
 
   system.stateVersion = "24.11";
 }
+
+
