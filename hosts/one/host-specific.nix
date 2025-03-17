2ce5301fb67e0482cc3098510b5ff6764e3d7541
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-17 11:46:11 +0100

    feat: add host aorus to syncthing

--- hosts/one/host-specific.nix
+++ hosts/one/host-specific.nix
@@ -116,7 +116,7 @@ in {
     cert = config.sops.secrets."syncthing-hosts/one/cert".path;
     settings.devices = {
       "elite" = {id = "OSGHMZK-KYEI6ET-F7B6GBM-5B4FBU2-U5LA4J4-T5KIRBY-PPPW3UX-5AZD4AS";};
-      # "aorus" = {id = "";};
+      "aorus" = {id = "FSFRLFB-FPC2GX6-H34V4YQ-ZS5FJ3M-NT6IZF5-567G3JA-CHBDLUN-TTCUUAN";};
     };
     settings.folders.Documents.devices = lib.mkForce ["elite"];
     settings.folders.Music.devices = lib.mkForce ["elite"];
