94cc3bffa0664470802ab3bd18a7f66b1c03184b
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-27 16:43:56 +0100

    feat: set environment variables to comply with XDG

--- home/rafael/environment/xdg.nix
+++ home/rafael/environment/xdg.nix
@@ -4,5 +4,14 @@
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_STATE_HOME = "$HOME/.local/state";
     XDG_CACHE_HOME = "$HOME/.cache";
+
+    TLDR_CACHE_DIR = "$XDG_CACHE_HOME/tldr";
+    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    GNUPGHOME = "XDG_DATA_HOME/gnupg";
+    GTK2_RC_FILES = "$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
+    IPYTHONDIR = "XDG_CONFIG_HOME/ipython";
+    JUPYTER_CONFIG_DIR = "XDG_CONFIG_HOME/jupyter";
+    DVDCSS_CACHE = "XDG_DATA_HOME/dvdcss";
+    MPLAYER_HOME = "XDG_CONFIG_HOME/mplayer";
   };
 }
