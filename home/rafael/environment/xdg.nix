0a75c911ca5e038d30f7206c81d60f54d15cca03
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-28 12:42:47 +0100

    fix: set environment variables correctly

--- home/rafael/environment/xdg.nix
+++ home/rafael/environment/xdg.nix
@@ -1,18 +1,18 @@
 { lib, ... }:
 {
-  home.sessionVariables = {
+  home.sessionVariables = rec {
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_STATE_HOME = "$HOME/.local/state";
     XDG_CACHE_HOME = "$HOME/.cache";
 
-    TLDR_CACHE_DIR = "$XDG_CACHE_HOME/tldr";
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
-    GNUPGHOME = lib.mkForce "XDG_DATA_HOME/gnupg";
-    GTK2_RC_FILES = lib.mkForce "$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
-    IPYTHONDIR = "XDG_CONFIG_HOME/ipython";
-    JUPYTER_CONFIG_DIR = "XDG_CONFIG_HOME/jupyter";
-    DVDCSS_CACHE = "XDG_DATA_HOME/dvdcss";
-    MPLAYER_HOME = "XDG_CONFIG_HOME/mplayer";
+    TLDR_CACHE_DIR = "${XDG_CACHE_HOME}/tldr";
+    CARGO_HOME = "${XDG_DATA_HOME}/cargo";
+    GNUPGHOME = lib.mkForce "${XDG_DATA_HOME}/gnupg";
+    GTK2_RC_FILES = lib.mkForce "${XDG_CONFIG_HOME}/gtk-2.0/gtkrc";
+    IPYTHONDIR = "${XDG_CONFIG_HOME}/ipython";
+    JUPYTER_CONFIG_DIR = "${XDG_CONFIG_HOME}/jupyter";
+    DVDCSS_CACHE = "${XDG_DATA_HOME}/dvdcss";
+    MPLAYER_HOME = "${XDG_CONFIG_HOME}/mplayer";
   };
 }
