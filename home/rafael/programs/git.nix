16b712da5bb6bab650634c3a68832846f216ffa2
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-06-11 17:05:09 +0200

    feat: auto sign git commits

--- home/rafael/programs/git.nix
+++ home/rafael/programs/git.nix
@@ -3,6 +3,12 @@
     userName = "Rafael Alexander Muijsert";
     userEmail = "rafael@muijsert.org";
 
+    signing = {
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
