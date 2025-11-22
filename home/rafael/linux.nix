14e02a8a5caff3ab1f0f662b8fbd9063faef1870
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-11-22 14:21:37 +0100

    move zellij to home-manager config

--- home/rafael/linux.nix
+++ home/rafael/linux.nix
@@ -33,6 +33,7 @@
       git.enable = true;
       helix.enable = true;
       yazi.enable = true;
+      zellij.enable = true;
       zoxide.enable = true;
     };
 
--- system/packages/default.nix
+++ system/packages/default.nix
@@ -15,6 +15,5 @@
     tldr
     tmux
     wget
-    zellij
   ];
 }
