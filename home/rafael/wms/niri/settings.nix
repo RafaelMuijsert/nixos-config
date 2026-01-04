405cdf4f0fe2113ce9e42f9cf3933e868075eb4c
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-01-04 15:44:27 +0100

    update border, overflow behavior

--- home/rafael/wms/niri/settings.nix
+++ home/rafael/wms/niri/settings.nix
@@ -74,7 +74,12 @@
             preset-column-widths = [
               { proportion = 1. / 2.; }
             ];
+            border = {
+              enable = true;
+              width = 2;
+            };
             always-center-single-column = true;
+            center-focused-column = "on-overflow";
             default-column-width = { proportion = 1. / 2.; };
           };
           outputs = {
