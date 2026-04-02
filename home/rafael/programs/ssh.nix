2c72082875f37e48a94f04537e8ca280bff3fe6c
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-04-02 13:24:56 +0200

    update ssh settings

---
+++ home/rafael/programs/ssh.nix
@@ -0,0 +1,15 @@
+{
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "zero" = {
+        hostname = "192.168.42.2";
+        user = "rafael";
+      };
+      "one" = {
+        hostname = "192.168.42.3";
+        user = "rafael";
+      };
+    };
+  };
+}
