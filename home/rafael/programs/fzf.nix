d90179028dc016f94d3606d4f41329c9c6438738
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-04-02 15:59:47 +0200

    configure fzf

--- home/rafael/programs/fzf.nix
+++ home/rafael/programs/fzf.nix
@@ -1,5 +1,8 @@
+{pkgs, ...}:
 {
   programs.fzf = {
     enableFishIntegration = true;
+    defaultCommand = "${pkgs.fd}/bin/fd --type file";
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type file";
   };
 }
