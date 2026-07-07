a4911f99fb1ac19d09e52643c5d9ddb3d962a310
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-07-07 21:16:54 +0200

    add syncthing hosts

--- modules/system/sync/default.nix
+++ modules/system/sync/default.nix
@@ -3,6 +3,7 @@
     nixos = {
       services.syncthing = {
         enable = true;
+        openDefaultPorts = true;
         overrideDevices = false;
         overrideFolders = true;
       };
@@ -16,10 +17,23 @@
     inherit user;
     dataDir = "${home}/.local/state/syncthing";
     configDir = "${home}/.config/syncthing";
+    settings.devices = {
+      one.id = "37VH62T-5UP3BSB-OKYPGOY-IGXFVFU-DHXXQQ3-3GW6INY-YTQJJYR-XXXGEQB";
+      aorus.id = "FSFRLFB-FPC2GX6-H34V4YQ-ZS5FJ3M-NT6IZF5-567G3JA-CHBDLUN-TTCUUAN";
+    };
     settings.folders = {
-      "Documents".path = "${home}/Documents";
-      "Music".path = "${home}/Music";
-      "Pictures".path = "${home}/Pictures";
+      "Documents" = {
+        path = "${home}/Documents";
+        devices = ["one" "aorus"];
+      };
+      "Music" = {
+        path = "${home}/Music";
+        devices = ["one" "aorus"];
+      };
+      "Pictures" = {
+        path = "${home}/Pictures";
+        devices = ["one" "aorus"];
+      };
     };
   };
 }
