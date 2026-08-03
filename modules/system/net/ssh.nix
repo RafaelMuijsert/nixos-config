98ee559f99c740aaf9ce93ce2ded27f17b93a5fb
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-08-03 11:18:58 +0200

    configure ssh hosts

--- modules/system/net/ssh.nix
+++ modules/system/net/ssh.nix
@@ -1,11 +1,35 @@
 {
-  den.ful.net.ssh.nixos.services.openssh = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      KbdInteractiveAuthentication = false;
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
+  den.ful.net.ssh = {
+    nixos.services.openssh = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        KbdInteractiveAuthentication = false;
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+    homeManager.programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "zero" = {
+          hostname = "zero.internal";
+          user = "rafael";
+        }:
+        "prox" = {
+          hostname = "prox.internal";
+          user = "rafael";
+        };
+        "infra" = {
+          hostname = "infra.internal";
+          user = "rafael";
+        };
+        "core" = {
+          hostname = "core.internal";
+          user = "rafael";
+        };
+      };
     };
   };
 }
