079d85c23e728ee3368f07e7671035039a85aa1d
Rafael Alexander Muijsert <rafael@muijsert.org>
2026-08-31 11:02:05 +0200

    update email addresses

--- modules/system/users/rafael/accounts.nix
+++ modules/system/users/rafael/accounts.nix
@@ -16,20 +16,7 @@
       };
     };
     Student = {
-      address = "rafael.muijsert@student.uva.nl";
-      flavor = "outlook.office365.com";
-      realName = "Rafael Alexander Muijsert";
-
-      thunderbird = {
-        enable = true;
-        settings = id: {
-          "mail.smtpserver.smtp_${id}.authMethod" = 10;
-          "mail.server.server_${id}.authMethod" = 10;
-        };
-      };
-    };
-    Work = {
-      address = "r.a.muijsert@uva.nl";
+      address = "r.a.muijsert@student.tudelft.nl";
       flavor = "outlook.office365.com";
       realName = "Rafael Alexander Muijsert";
 
